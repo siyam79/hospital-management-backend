42365bc192950d3f87089e52acf2b3b7a56368dd
siyam79 <siyamahmed3827@gmail.com>
2024-08-08 14:53:28 +0600

    apiError and apiResaaponse utils discussion

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 import dotenv from "dotenv"
 dotenv.config({
-    path : "./env"
+    path: "./env"
 })
 
 connectDB()
+    .then(()=>{
+        app.listen(process.env.PORT || 5000 , ()=> {
+            console.log(`Server is runging at port : ${process.env.PORT} `);
+        })
+    })
+    .catch((error) => {
+        console.log("Mongo DB Connection is Feild !!!" , error);
+    })
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,6 @@
+export const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+            .catch((error) => next(error))
+    }
+}
